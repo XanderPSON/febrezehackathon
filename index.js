b069edce8a3e82dc64e34678db4dd7fe5c508aa3
XanderPSON <xwpeterson@gmail.com>
2017-03-04 15:05:19 -0800

    Add info on setting parameters in route

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ app.put('/user', function (req, res) {
 app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user')
 })
+
+// Route path: /users/:userId/books/:bookId
+// Request URL: http://localhost:3000/users/34/books/8989
+// req.params: { "userId": "34", "bookId": "8989" }
+// To define routes with route parameters, simply specify the route parameters in the path of the route as shown below.
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
