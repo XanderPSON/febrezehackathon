c0abae6eef06927b21783c26cc835fb9a3839afa
jabari <jabaricmartin@gmail.com>
2017-03-05 12:43:26 -0800

    postman stuff

--- app/javascript/nest.js
+++ app/javascript/nest.js
@@ -19,15 +19,31 @@
 var febreze = {};
 
 febreze.attr = {
-  body: [{"DeviceAction": "led_behavior=28" }],
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer c.7rvd9nnU6bK1kxsZqheeMibmH2XiDKxp2kSs75oZQlYbJ56pWWuh2c1NVj98w8jEUc8b1t4sDUB34qROwArIaM3xhUaVzUMLuX2KULTZsgUM1q4tJ5grFgF1XyDdW60hbt1rjhOBN3A1J21K",
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With'
+  "async": true,
+  "crossDomain": true,
+  "url": "https://na-hackathon-api.arrayent.io:443/v3/devices/50331658",
+  "method": "GET",
+  "headers": {
+    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJjMjljY2ExMC0wMTMxLTExZTctOTIwNy1iNWMzYjY2M2Y2YTQiLCJlbnZpcm9ubWVudF9pZCI6Ijk0OGUyY2YwLWZkNTItMTFlNi1hZTQ2LTVmYzI0MDQyYTg1MyIsInVzZXJfaWQiOiI5MDAwMDg1Iiwic2NvcGVzIjoie30iLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwiaWF0IjoxNDg4NjcwOTA0LCJleHAiOjE0ODk4ODA1MDR9.k4OUi-vWga96-TqxpwGiJ-Ml0_faVofaumkI94m0tIyB2IYshNnElZD7pL0DFdW1M-x00HwKSBVJRo7S8JpbJQ",
+    "cache-control": "no-cache",
+    "postman-token": "4027879d-38ab-c5ad-81cb-ce75afe30332"
   }
 };
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://na-hackathon-api.arrayent.io:443/v3/devices/50331658",
+  "method": "GET",
+  "headers": {
+    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJjMjljY2ExMC0wMTMxLTExZTctOTIwNy1iNWMzYjY2M2Y2YTQiLCJlbnZpcm9ubWVudF9pZCI6Ijk0OGUyY2YwLWZkNTItMTFlNi1hZTQ2LTVmYzI0MDQyYTg1MyIsInVzZXJfaWQiOiI5MDAwMDg1Iiwic2NvcGVzIjoie30iLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwiaWF0IjoxNDg4NjcwOTA0LCJleHAiOjE0ODk4ODA1MDR9.k4OUi-vWga96-TqxpwGiJ-Ml0_faVofaumkI94m0tIyB2IYshNnElZD7pL0DFdW1M-x00HwKSBVJRo7S8JpbJQ",
+    "cache-control": "no-cache",
+    "postman-token": "4027879d-38ab-c5ad-81cb-ce75afe30332"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 febreze.env = {
   scheme: "https",
@@ -150,7 +166,7 @@ function listenForSmokeAlarms(alarm) {
 }
 
 function activate_spray(alarm) {
-  febreze.checkDeviceStatus();
+  //febreze.checkDeviceStatus();
 
   console.log(alarm.child('ui_color_state').val(), alarm.child('smoke_alarm_state').val())
 }
