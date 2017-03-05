7e8fea99e0784a02dd46bcd5963d9ab51191b8f8
XanderPSON <xwpeterson@gmail.com>
2017-03-05 08:58:00 -0800

    Use node-sdk to authenticate

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
 })
 
 app.get('/', function (req, res) {
